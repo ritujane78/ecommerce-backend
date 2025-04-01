040b9d5bc705f4ab73a83b684e708af9033fa129
Ritu Bafna <ritujane78@gmail.com>
2025-04-01 16:57:19 +0545

    only expose annotated repositories

--- src/main/java/com/jane/ecommerce/dao/StateRepository.java
+++ src/main/java/com/jane/ecommerce/dao/StateRepository.java
@@ -3,9 +3,11 @@ package com.jane.ecommerce.dao;
 import com.jane.ecommerce.entity.State;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource
 public interface StateRepository extends JpaRepository<State, Integer> {
     List<State> findByCountryCode(@Param("code") String code);
 }
