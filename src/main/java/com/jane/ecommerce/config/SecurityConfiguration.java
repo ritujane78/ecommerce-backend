e90c87aff63adf33fe2aa8a843c7d26d802aaff3
Ritu Bafna <ritujane78@gmail.com>
2025-04-17 12:52:37 +0545

    update code with newer okta version and newer security config

--- src/main/java/com/jane/ecommerce/config/SecurityConfiguration.java
+++ src/main/java/com/jane/ecommerce/config/SecurityConfiguration.java
@@ -1,10 +1,11 @@
 package com.jane.ecommerce.config;
-
 import com.okta.spring.boot.oauth.Okta;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.web.accept.ContentNegotiationStrategy;
 import org.springframework.web.accept.HeaderContentNegotiationStrategy;
 
@@ -12,26 +13,30 @@ import org.springframework.web.accept.HeaderContentNegotiationStrategy;
 public class SecurityConfiguration {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        // protect endpoint /api/orders
-        http
-                .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/api/orders/**").authenticated()
-                        .anyRequest().permitAll()
-                )
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt());
+        //protect endpoint /api/orders
+        http.authorizeHttpRequests(requests ->
+                        requests
+                                .requestMatchers("/api/orders/**")
+                                .authenticated()
+                                .anyRequest().permitAll())
+                .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt(Customizer.withDefaults()));
 
-        // add CORS filters
-        http.cors();
+        // + CORS filters
+        http.cors(Customizer.withDefaults());
 
-        // add content negotiation strategy
-        http.setSharedObject(ContentNegotiationStrategy.class,
-                new HeaderContentNegotiationStrategy());
+        // + content negotiation strategy
+        http.setSharedObject(ContentNegotiationStrategy.class, new HeaderContentNegotiationStrategy());
 
-        // force a non-empty response body for 401's to make the response more friendly
+        // + non-empty response body for 401 (more friendly)
         Okta.configureResourceServer401ResponseBody(http);
+
+        // we are not using Cookies for session tracking >> disable CSRF
+        http.csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
+
 }
+
