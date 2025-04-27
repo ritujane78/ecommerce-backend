d7047009c9a2a2eaafc8a950416074bc777fe020
Ritu Bafna <ritujane78@gmail.com>
2025-04-27 08:54:35 +0545

    send email receipt from stripe to the payer

--- src/main/java/com/jane/ecommerce/dto/PaymentInfo.java
+++ src/main/java/com/jane/ecommerce/dto/PaymentInfo.java
@@ -3,6 +3,17 @@ package com.jane.ecommerce.dto;
 public class PaymentInfo {
     private int amount;
     private String currency;
+    private String receiptEmail;
+
+    public String getReceiptEmail() {
+        return receiptEmail;
+    }
+
+    public void setReceiptEmail(String receiptEmail) {
+        this.receiptEmail = receiptEmail;
+    }
+
+
 
     public int getAmount() {
         return amount;
